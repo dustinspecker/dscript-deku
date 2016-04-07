3b02f9832f2c0bcf0ce03f91bed61df49afddae5
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-04-07 16:03:33 -0500

    chore(deps): update AVA

--- tests/test.js
+++ tests/test.js
@@ -7,6 +7,6 @@ test('verify it works with Deku\'s element', t => {
   const dekuDiv = div('.yo#hi', {title: 'hello'}, ['world'])
 
   t.is(dekuDiv.type, 'div')
-  t.same(dekuDiv.attributes, {title: 'hello', class: 'yo', id: 'hi'})
-  t.same(dekuDiv.children[0], {type: '#text', nodeValue: 'world'})
+  t.deepEqual(dekuDiv.attributes, {title: 'hello', class: 'yo', id: 'hi'})
+  t.deepEqual(dekuDiv.children[0], {type: '#text', nodeValue: 'world'})
 })
